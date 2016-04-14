e9823521384690b458ea953488b3aeed0d33c66e
Jayanth Prathipati <jayanth@vt.edu>
2016-04-14 19:06:06 -0400

    test

--- facebook_bot.js
+++ facebook_bot.js
@@ -142,38 +142,36 @@ controller.hears(['food'], 'message_received', function(bot, message) {
     bot.startConversation(message, function(err, convo) {
         convo.ask('Where do you want me to find food?', function(response, convo) {
             bot.reply(message, 'okay, looking for restaurants...give me a second'); 
-            yelp.search({ term: 'food', location: response.text })
+            yelp.search({ term: 'food', location: response.text})
                 .then(function (data) {
 
                 var randNum = randomIntInc(0, data.businesses.length)
 
                   // bot.reply(message, 'Here is a restaurant for you: ' + data.businesses[randNum].name + ' ' + data.businesses[randNum].url); 
 
-                  bot.reply(message, {
-                      attachment: {
-                          'type': 'template',
-                          'payload': {
-                              'template_type': 'generic',
-                              'elements': [
-                                  {
-                                      'title': data.businesses[randNum].name,
-                                      'image_url': data.businesses[randNum].image_url,
-                                      'subtitle': data.businesses[randNum].location.address[0],
-                                      'buttons': [
-                                          {
-                                              'type': 'web_url',
-                                              'url': data.businesses[randNum].url,
-                                              'title': 'View restaurant'
-                                          }
-                                      ]
-                                  }
-                              ]
+                      bot.reply(message, {
+                          attachment: {
+                              'type': 'template',
+                              'payload': {
+                                  'template_type': 'generic',
+                                  'elements': [
+                                      {
+                                          'title': data.businesses[randNum].name,
+                                          'image_url': data.businesses[randNum].image_url,
+                                          'subtitle': data.businesses[randNum].location.address[0],
+                                          'buttons': [
+                                              {
+                                                  'type': 'web_url',
+                                                  'url': data.businesses[randNum].url,
+                                                  'title': 'View restaurant'
+                                              }
+                                          ]
+                                      }
+                                  ]
+                            }
                         }
-                    }
-                });
-
-
-
+                    });
+                    convo.next(); 
 
                 })
                 .catch(function (err) {
